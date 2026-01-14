c80534ccd7f5de5cd8c03b026ca037f6c2d1cd38
Oleks Zolochevskyi <fastzolochevskiy@gmail.com>
2026-01-14 13:52:12 -0600

    vb

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,7 +29,8 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
+		robot.forward(120);
+		robot.
 		robot.turnLeft(90);
 		robot.forward(23);
 		robot.turnRight(90);
